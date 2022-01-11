de78db12b9bf7feff13c0e4df907fc894a2f044e
Zarchan <zachary.c.me@gmail.com>
2022-01-11 11:59:07 -0800

    Fix copy/paste error

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ with open("README.md", "r") as readme_file:
 requirements = []
 
 setup(
-    name="keyval",
-    version="0.0.7",
+    name="ACNH_pixel_color",
+    version="0.0.1",
     author="Zachary Chandler",
     author_email="zachary.c.me@gmail.com",
     description="A program to associate keys in one text file with values in another",
     license="MIT",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/burnish3d/keyval",
+    url="https://github.com/burnish3d/ACNH_pixel_color",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
