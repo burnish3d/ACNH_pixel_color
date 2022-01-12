664680ade0e15ce596f54bbfd870c8e31ad607ca
Zarchan <zachary.c.me@gmail.com>
2022-01-11 21:54:02 -0800

    path?

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = []
 
 setup(
     name="ACNH_pixel_color",
-    version="0.1.0",
+    version="0.1.1",
     author="Zachary Chandler",
     author_email="zachary.c.me@gmail.com",
     description="A program to associate IDs with h/v/b values for color matching in ACNH",
