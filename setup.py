052c9d7755b4e2521ada2d73a27413e57a2e77ac
Zarchan <zachary.c.me@gmail.com>
2022-01-11 21:45:08 -0800

    Packaged for deployment

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = []
 
 setup(
     name="ACNH_pixel_color",
-    version="0.0.8",
+    version="0.1.0",
     author="Zachary Chandler",
     author_email="zachary.c.me@gmail.com",
     description="A program to associate IDs with h/v/b values for color matching in ACNH",
@@ -18,6 +18,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=requirements,
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
